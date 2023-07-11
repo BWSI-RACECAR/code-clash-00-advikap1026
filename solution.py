af352679bd9da5cdef6155797511213705dda5af
Advika Prashanth <advika.prashanth08@gmail.com>
2023-07-10 20:51:49 -0400

    Update solution.py

---
+++ solution.py
@@ -0,0 +1,18 @@
+class Solution:
+    def helloworld(self, string):
+        #input type: String
+        # return type: String
+        
+        # TODO: Write code below to return a string with the solution to the prompt
+        if string == "Hello World!":
+            return "Hello World!"
+        pass
+
+def main():
+    tc1 = Solution()
+    string1= input()
+    ans = tc1.helloworld(string1)
+    print(ans)
+
+if __name__ == "__main__":
+    main()
